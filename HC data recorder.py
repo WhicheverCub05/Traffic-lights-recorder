5a639124c40b755af4046356c935b64a8dbe0436
Alex <44294722+WhicheverCub05@users.noreply.github.com>
2019-07-31 10:49:30 +0100

    Update HC data recorder.py

--- HC data recorder.py
+++ HC data recorder.py
@@ -1,13 +1,15 @@
-from PIL import Image
 from PIL import ImageGrab
 import pytesseract
-import argparse
 import cv2
-import os
 import time
 import numpy as np
+import pyautogui
+
+#  Available Libraries = os, argparse, Image
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\Tesseract.exe'
 
+tempList = []
 
 ocr_figure = []
 ocr_figureList = []
@@ -30,25 +32,78 @@ def display_image(image):
 
 def image_grab(number):
     if number == 1:
-        capture = ImageGrab.grab(bbox=(970, 186, 1017, 206))
+        capture = ImageGrab.grab(bbox=(820, 186, 891, 206))
         capture_np = np.array(capture)
 
-    if number == 2:
-        capture = ImageGrab.grab(bbox=(445, 186, 510, 206))
+    elif number == 2:
+        capture = ImageGrab.grab(bbox=(320, 186, 380, 206))
         capture_np = np.array(capture)
 
-    if number == 3:
-        capture = ImageGrab.grab(bbox=(172, 166, 242, 186))
+    elif number == 3:
+        capture = ImageGrab.grab(bbox=(37, 164, 110, 190))
         capture_np = np.array(capture)
+
+    elif number == 4:  # actual runtime
+        capture = ImageGrab.grab(bbox=(37, 124, 110, 150))
+        capture_np = np.array(capture)
+
+    elif number == 5:  # finished packs
+        capture = ImageGrab.grab(bbox=(852, 640, 953, 665))
+        capture_np = np.array(capture)
+
+    elif number == 6:  # finished cases
+        capture = ImageGrab.grab(bbox=(1460, 640, 1503, 665))
+        capture_np = np.array(capture)
+
+    else:
+        capture = ImageGrab.grab(bbox=(0, 0, 1, 1))
+        capture_np = np.array(capture)
+        print("nothing was read")
 
     return capture_np
 
 
+def mouse_click(x, y):
+    pyautogui.moveTo(x, y, duration=0.25)
+    pyautogui.click()
+
+
+def keyboard_type(key):
+    pyautogui.typewrite(key, 0.25)
+
+
+def increment_time(starting_min):
+    # OEE testbed
+    mouse_click(637, 77)
+    time.sleep(0.25)
+    keyboard_type('0{}'.format(starting_min))
+    mouse_click(720, 70)
+    time.sleep(2)
+    # TO Finished packs
+    mouse_click(637, 495)
+    time.sleep(0.25)
+    keyboard_type('0{}'.format(starting_min))
+    mouse_click(720, 485)
+    time.sleep(2)
+    # TO Finished cases
+    mouse_click(1120, 495)
+    time.sleep(0.25)
+    keyboard_type('0{}'.format(starting_min))
+    mouse_click(1205, 485)
+    time.sleep(2)
+
+
 def ocr(image):
-    return pytesseract.image_to_string(image)
+    value = pytesseract.image_to_string(image)
+    print('value: ', value)
+    if value == ' ':
+        value = '0'
+        return value
+    else:
+        return value
 
 
-
+'''
 def correct_value(ocr_character):
     value = ocr_character
     value.replace(' ', '')  # removes spaces
@@ -56,25 +111,110 @@ def correct_value(ocr_character):
     return value
 
 
-def correct_list(value):
-    for i in range(0, len(ocr_figureList)-1):
-        for i in range(0, len(ocr_figure)-1):
+def correct_list():
+    for i in range(0, len(ocr_figureList)):
+        for j in range(0, 1):
+            value = correct_value(ocr_figure[j+i])
+            number_figure.append(value)
+        number_figureList.append(number_figure)
+    print("number_figureList: ", number_figureList)
+'''
+
+
+def write_to_excel(values, iterations, value_count):
+    mouse_click(350, 695)
+    hInc = 70
+    vInc = 15
+    for i in range(0, iterations):
+        mouse_click(350+(i*hInc), 695)
+        for j in range(0, value_count):  # len(number_figureList)
+            mouse_click(350+(i*hInc), 695+((j % value_count)*vInc))
+            keyboard_type('{}'.format(values[i][j]))
+
+
+def make_excel_list(values, iterations, value_count):
+    for i in range(0, ((len(values)) // value_count)):
+        listCounter= i * value_count
+        for j in range(0, value_count):
+            tempList.append(values[listCounter])
+            listCounter += 1
+        ocr_figureList.append(tempList)
+        print('ocr: ', ocr_figureList)
+        if len(ocr_figureList) == iterations:
+            return ocr_figureList
+        else:
             pass
-        pass
+        del tempList[0:value_count]
 
 
-def test_sequence():
-    for i in range(0, 2):
-        image_to_process = image_grab(i+1)
-        print(ocr(image_to_process))
-        display_image(image_to_process)
+def main_sequence(iterations, interval_time):
+    for i in range(0, iterations):
+        increment_time(i)
+        time.sleep(interval_time)
+        for j in range(0, value_count):
+            image = image_grab(j+1)
+            value = ocr(image)
+            #display_image(image)
+            ocr_figure.append(value)
 
-        ocr_figure.append(ocr_figure)
-    ocr_figureList.append(ocr_figure)
 
+def the_thing():
+    interval_time = 2
+    iterations = int(2)
+    value_count = int(6)
+    main_sequence(iterations, interval_time)
+    usableList = make_excel_list(ocr_figure, iterations, value_count)
+    write_to_excel(usableList, iterations, value_count)
+
+
+while True:
+    answer = input('do the thing? y/n: ')
+    try:
+        answer = str(answer)
+    except:
+        print('input a letter')
+    if answer == 'y':
+        print('Terminate the program by pressing CTRL+C')
+        time.sleep(5)
+        the_thing()
+    elif answer == 'n':
+        print('closing program')
+        time.sleep(2)
+        break
+    else:
+        print('enter the correct input')
 
 
 '''
+main_sequence(iterations, interval_time)
+usableList = make_excel_list(ocr_figure, iterations, value_count)
+print(usableList)
+write_to_excel(usableList, iterations, value_count)
+'''
+# ssssssssssssssssssssssssssssssssssssssssssssssssssssss
+
+'''
+def test_sequence():
+    time_increment = 0
+    for j in range(0, 3):
+        image_to_process = image_grab(j+1)
+        ocr_value = (ocr(image_to_process))
+        display_image(image_to_process)
+        ocr_figure.append(ocr_value)
+    ocr_figureList.append(ocr_figure)
+    time_increment += 1
+    time.sleep(2)
+    increment_time(time_increment % 60)
+    print("ocr_figureList: ", ocr_figureList)
+
+
+for i in range(0, 2):
+    test_sequence()
+correct_list()
+write_to_excel()
+
+
+
 while not keybreak:
     k = cv2.waitKey(0)
     image1 = open_image("image.png")
